7148a4a4af0bdf38302cdee7177b8dca2efc1a84
KAY <slimshades68@gmail.com>
2020-04-10 20:54:08 +0000

    Challenge 1 - 3

--- src/xml.php
+++ src/xml.php
@@ -19,5 +19,5 @@ function print_xml($data)
 $time2 = microtime(true);
 
 $exe_time = ($time2- $_SERVER["REQUEST_TIME_FLOAT"])* 1000;
-$logMessage = $_SERVER['REQUEST_METHOD']. "\t\t".$_SERVER['REQUEST_URI']. "\t\t".http_response_code()."\t\t". round($exe_time,2)."ms";
+$logMessage = $_SERVER['REQUEST_METHOD']. "\t\t".$_SERVER['REQUEST_URI']. "\t\t".http_response_code()."\t\t". round($exe_time,2)." ms";
 file_put_contents('logs.txt', $logMessage."\n", FILE_APPEND | LOCK_EX);
