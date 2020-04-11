6c2b5ffa4b30c21448909167ac2e775c74d98224
KAY <slimshades68@gmail.com>
2020-04-11 04:26:30 +0000

    Challenge 1 - 3

--- src/xml.php
+++ src/xml.php
@@ -3,55 +3,11 @@ header("Content-Type: text/xml");
 include "estimator.php";
 
 
-//echo hhb_xml_encode(covid19ImpactEstimator($decoded));
-
-
-
-
-echo xml_encode(covid19ImpactEstimator($decoded));
-function xml_encode($mixed, $domElement=null, $DOMDocument=null) {
-    if (is_null($DOMDocument)) {
-        $DOMDocument =new DOMDocument;
-        $DOMDocument->formatOutput = true;
-        xml_encode($mixed, $DOMDocument, $DOMDocument);
-        return $DOMDocument->saveXML();
-    }
-    else {
-        if (is_array($mixed)) {
-            foreach ($mixed as $index => $mixedElement) {
-                if (is_int($index)) {
-                    if ($index === 0) {
-                        $node = $domElement;
-                    }
-                    else {
-                        $node = $DOMDocument->createElement($domElement->tagName);
-                        $domElement->parentNode->appendChild($node);
-                    }
-                }
-                else {
-                    $plural = $DOMDocument->createElement($index);
-                    $domElement->appendChild($plural);
-                    $node = $plural;
-                }
-
-                xml_encode($mixedElement, $node, $DOMDocument);
-            }
-        }
-        else {
-            $domElement->appendChild($DOMDocument->createTextNode($mixed));
-        }
-    }
-}
-
-
-$time2 = microtime(true);
-
-$exe_time = ($time2- $_SERVER["REQUEST_TIME_FLOAT"])* 1000;
-$logMessage = $_SERVER['REQUEST_METHOD']. "\t\t".$_SERVER['REQUEST_URI']. "\t\t".http_response_code()."\t\t". round($exe_time,2)." ms";
-file_put_contents('logs.txt', $logMessage."\n", FILE_APPEND | LOCK_EX);
+echo hhb_xml_encode(covid19ImpactEstimator($decoded));
 
 
 function hhb_xml_encode(array $arr, string $name_for_numeric_keys = 'val'): string {
+
     if (empty ( $arr )) {
         return '';
     }
@@ -63,34 +19,9 @@ function hhb_xml_encode(array $arr, string $name_for_numeric_keys = 'val'): stri
             return false;
         return array_keys ( $arr ) !== range ( 0, count ( $arr ) - 1 );
     };
-    $endsWith = function (string $haystack, string $needle): bool {
-        $length = strlen ( $needle );
-        if ($length == 0) {
-            return true;
-        }
-        return (substr ( $haystack, - $length ) === $needle);
-    };
-    $formatXML = function (string $xml) use ($endsWith): string {
-        $domd = new DOMDocument ( '1.0', 'UTF-8' );
-        $domd->preserveWhiteSpace = false;
-        $domd->formatOutput = true;
-        $domd->loadXML ( '<root>' . $xml . '</root>' );
-        $ret = trim ( $domd->saveXML ( $domd->getElementsByTagName ( "root" )->item ( 0 ) ) );
-        $full = trim ( substr ( $ret, strlen ( '<root>' ), - strlen ( '</root>' ) ) );
-        $ret = '';
-        foreach ( explode ( "\n", $full ) as $line ) {
-            if (substr ( $line, 0, 2 ) === '  ') {
-                $ret .= substr ( $line, 2 ) . "\n";
-            } else {
-                $ret .= $line . "\n";
-            }
-        }
-        $ret = trim ( $ret );
-        return $ret;
-    };
 
     $iterator = $arr;
-    $domd = new DOMDocument ();
+    $domd = new DOMDocument("1.0", "utf-8");
     $root = $domd->createElement ( 'root' );
     foreach ( $iterator as $key => $val ) {
 
@@ -100,7 +31,7 @@ function hhb_xml_encode(array $arr, string $name_for_numeric_keys = 'val'): stri
                 $asoc = $isAssoc ( $val );
                 $tmp = hhb_xml_encode ( $val, is_int ( $key ) ? $name_for_numeric_keys : $key );
 
-                $tmp = @DOMDocument::loadXML ( '<root>' . $tmp . '</root>' );
+                $tmp = @DOMDocument::loadXML (  $tmp  );
                 foreach ( $tmp->getElementsByTagName ( "root" )->item ( 0 )->childNodes ?? [ ] as $tmp2 ) {
                     $tmp3 = $domd->importNode ( $tmp2, true );
                     if ($asoc) {
@@ -109,6 +40,7 @@ function hhb_xml_encode(array $arr, string $name_for_numeric_keys = 'val'): stri
                         $root->appendChild ( $tmp3 );
                     }
                 }
+
                 unset ( $tmp, $tmp2, $tmp3 );
                 if (! $asoc) {
 
@@ -122,10 +54,16 @@ function hhb_xml_encode(array $arr, string $name_for_numeric_keys = 'val'): stri
     }
     $domd->preserveWhiteSpace = false;
     $domd->formatOutput = true;
-    $ret = trim ( $domd->saveXML ( $root ) );
-    $ret = trim ( substr ( $ret, strlen ( '<root>' ), - strlen ( '</root>' ) ) );
 
-    $ret = $formatXML ( $ret );
-    /*return '<?xml version="1.0" ?><root>' . $ret . '</root>';*/
-    return $ret;
+    $ret = $domd->saveXML ( $root );
+    return '<?xml version="1.0" encoding="UTF-8"?>' . $ret ;
 }
+
+
+$time2 = microtime(true);
+
+$exe_time = ($time2- $_SERVER["REQUEST_TIME_FLOAT"])* 1000;
+$logMessage = $_SERVER['REQUEST_METHOD']. "\t\t".$_SERVER['REQUEST_URI']. "\t\t".http_response_code()."\t\t". round($exe_time,2)." ms";
+file_put_contents('logs.txt', $logMessage."\n", FILE_APPEND | LOCK_EX);
+
+
