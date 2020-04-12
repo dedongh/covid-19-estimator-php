0df01f89c9b8fc27fab2672d42325a501f674986
KAY <slimshades68@gmail.com>
2020-04-12 16:12:35 +0000

    Challenge 1 - 3

--- src/json.php
+++ src/json.php
@@ -13,6 +13,7 @@ function print_json($data)
 }
 $time2 = microtime(true);
 
-$exe_time = ($time2- $_SERVER["REQUEST_TIME_FLOAT"])* 1000;
-$logMessage = $_SERVER['REQUEST_METHOD']. "\t\t".$_SERVER['REQUEST_URI']. "\t\t".http_response_code()."\t\t". round($exe_time,2)."ms";
+$exe_time = "0".(int)($time2- $_SERVER["REQUEST_TIME_FLOAT"])* 1000;
+//$exe_time = "0".(int)($time2- $time1)* 1000;
+$logMessage = $_SERVER['REQUEST_METHOD']. "\t\t".$_SERVER['REQUEST_URI']. "\t\t".http_response_code()."\t\t". $exe_time."ms";
 file_put_contents('logs.txt', $logMessage."\n", FILE_APPEND | LOCK_EX);
